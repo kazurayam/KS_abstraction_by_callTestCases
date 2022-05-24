b455f4b6f0deedf8fd695f10c0722b09d302d2a3
kazurayam <kazuaki.urayama@gmail.com>
2022-05-24 11:00:15 +0900

    update Test Case/AVS

--- Scripts/AVS/Script1653353459381.groovy
+++ Scripts/AVS/Script1653353459381.groovy
@@ -1,10 +1,3 @@
-// https://www.baeldung.com/java-download-file
-
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.testobject.HttpBodyContent
-import com.kms.katalon.core.testobject.ResponseObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-
 String FILE_URL = "http://localhost:80/sample.docx"
 URL url = new URL(FILE_URL)
 InputStream inputStream = url.openStream()
